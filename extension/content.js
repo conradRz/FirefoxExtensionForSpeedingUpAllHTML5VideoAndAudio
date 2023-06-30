0872a9a0407fe4b6a1efee77eb00e669f5566ccd
Conrad <conrad.rzuc@gmail.com>
2023-06-30 04:16:10 +0100

    Update settings.js

--- extension/content.js
+++ extension/content.js
@@ -2,14 +2,10 @@ browser.runtime.onMessage.addListener(function (request) {
   switch (request.command) {
     case "speedUp025":
       return updateElementPlaybackRate(0.25);
-    case "resetSpeed":
-      return resetElementPlaybackRate();
     case "slowDown025":
       return updateElementPlaybackRate(-0.25);
   }
 });
-
-//put here DOM completed listener, and then set speed of AUDIO/VIDEO elements based on settings
 
 function updateElementPlaybackRate(changeValue) {
   return new Promise(function (resolve) {
@@ -27,25 +23,6 @@ function updateElementPlaybackRate(changeValue) {
     }
 
     element.playbackRate = newPlaybackRate;
-    resolve(element.playbackRate);
-  });
-}
-
-function resetElementPlaybackRate() {
-  return new Promise(function (resolve) {
-    var element = getCurrentlyPlayingElement();
-
-    if (typeof element === 'undefined') {
-      resolve();
-      return;
-    }
-
-    if (element.playbackRate === 1) {
-      resolve();
-      return;
-    }
-
-    element.playbackRate = 1;
     resolve(element.playbackRate);
   });
 }
